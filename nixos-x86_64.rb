7864f6ce7c735e8b54fd1c4a5a6683f15304ba9a
Parnell Springmeyer <parnell@digitalmentat.com>
2016-09-07 16:53:03 -0500

    Updating for my atlas user and the latest nixos release.

--- nixos-x86_64.rb
+++ nixos-x86_64.rb
@@ -3,7 +3,7 @@ require './gen_template.rb'
 
 gen_template(
   arch: "x86_64",
-  iso_url: "https://nixos.org/releases/nixos/16.03/nixos-16.03.1271.546618c/nixos-minimal-16.03.1271.546618c-x86_64-linux.iso",
-  iso_sha256: "81c91427d7fd384097a8a811715f69379a81c3da02c7db39ec85f4a11f2d42f0",
+  iso_url: "https://nixos.org/releases/nixos/16.03/nixos-16.03.1287.6ac7ffd/nixos-minimal-16.03.1287.6ac7ffd-x86_64-linux.iso",
+  iso_sha256: "3a250ff98773941bd44b4e84931882321b0dcbe3b4c8819590767dd7020a094d",
   virtualbox_guest_os: "Linux_64",
 )
